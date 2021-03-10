e2c03c369849b2a2c50f5d61f63e4c069531b905
Engku Nazri <engkunazri0609@gmail.com>
2021-03-10 14:57:27 +0800

    deploy

--- deploy.sh
+++ deploy.sh
@@ -17,9 +17,10 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
-git push -f git@github.com:panitiasejarah/panitiasejarah.github.io.git master
+#git push -f git@github.com:panitiasejarah/panitiasejarah.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
+git push -f git@github.com:panitiasejarah/panitiasejarah.git master:website
 
 cd -
